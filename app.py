44d7d054bbe377827c9669a694dad4bbb5b011cc
Kayy01 <kaurkareenajit@gmail.com>
2024-12-05 09:20:03 +0800

    app.py

--- app.py
+++ app.py
@@ -101,8 +101,6 @@ frontend_settings = {
         "chat_description": app_settings.ui.chat_description,
         "show_share_button": app_settings.ui.show_share_button,
         "show_chat_history_button": app_settings.ui.show_chat_history_button,
-         # Additional Frontend Settings (not via environment variables)
-        "show_dark_mode_button": app_settings.ui.show_dark_mode_button,
     },
     "sanitize_answer": app_settings.base_settings.sanitize_answer,
     "oyd_enabled": app_settings.base_settings.datasource_type,
